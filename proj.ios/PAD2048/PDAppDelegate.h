1bb10f79aad10dd4699a857c0ad6187e1fb6d507
sunzhuoshi <sunzhuoshi@gmail.com>
2014-03-30 10:22:11 +0800

    Add support for iOS

---
+++ proj.ios/PAD2048/PDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PDAppDelegate.h
+//  PAD2048
+//
+//  Created by sun zhuoshi on 3/27/14.
+//  Copyright (c) 2014 SpringPals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ proj.ios/PAD2048/PDViewController.h
@@ -0,0 +1,15 @@
+//
+//  PDViewController.h
+//  PAD2048
+//
+//  Created by sun zhuoshi on 3/27/14.
+//  Copyright (c) 2014 SpringPals. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PDViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIWebView* webView;
+
+@end
