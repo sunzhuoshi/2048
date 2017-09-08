9a124d1b597fb49626b9de42435b0b8f83922c1c
sunzhuoshi <sunzhuoshi@gmail.com>
2017-09-08 11:36:42 +0800

    Fix auto-play issue on iOS

--- js/keyboard_input_manager_ext.js
+++ js/keyboard_input_manager_ext.js
@@ -24,11 +24,13 @@ KeyboardInputManager.prototype.listen = function() {
 	this.bindButtonPress(".hint-button", this.showHint);
 }
 
-KeyboardInputManager.prototype.autoPlay = function() {
+KeyboardInputManager.prototype.autoPlay = function(event) {
+	event.preventDefault();
 	GameManager.instance().autoPlay(this.getAlgorithm());
 }
 
-KeyboardInputManager.prototype.showHint = function() {
+KeyboardInputManager.prototype.showHint = function(event) {
+	event.preventDefault();	
 	var bestMove = GameManager.instance().getBestMove(this.getAlgorithm());
 	if (0 <= bestMove) {
 		document.getElementById('hint').innerHTML = this.directionToSymbol(bestMove);		
